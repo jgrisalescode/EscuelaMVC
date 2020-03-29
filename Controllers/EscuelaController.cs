9bea45ce8bbe536a7a4f4cf5aabe982b423a6692
JuliaGrisales <jgrisales@gmail.com>
2020-03-29 13:25:21 +0200

    Lección 2: MVC Básico, creamdo nuestra primer vista y primer controlador (Escuela/Index, EscuelaController)

---
+++ Controllers/EscuelaController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EscuelaMVC.Controllers
+{
+    public class EscuelaController : Controller
+    {
+        public IActionResult Index()
+        {
+            // IActionResult es una interfáz genérica como IEnumerable, que luego podremos convertir
+            return View();
+            // Como return View(); no tiene parámetros retorna la vista Index
+        }
+    }
+}
