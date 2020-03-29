bb9364c250caa62cfdac84cd642dd2703272c07b
JuliaGrisales <jgrisales@gmail.com>
2020-03-29 19:03:22 +0200

    Lección 7: Creando Vistas Parciales

--- Controllers/AsignaturaController.cs
+++ Controllers/AsignaturaController.cs
@@ -19,5 +19,29 @@ namespace EscuelaMVC.Controllers
             ViewBag.Fecha = DateTime.Now;
             return View(asignatura);
         }
+
+        public IActionResult MultiAsignatura()
+        {
+            var listaAsignaturas = new List<Asignatura>(){
+                            new Asignatura{Nombre="Matemáticas",
+                                UniqueId= Guid.NewGuid().ToString()
+                            } ,
+                            new Asignatura{Nombre="Educación Física",
+                                UniqueId= Guid.NewGuid().ToString()
+                            },
+                            new Asignatura{Nombre="Castellano",
+                                UniqueId= Guid.NewGuid().ToString()
+                            },
+                            new Asignatura{Nombre="Ciencias Naturales",
+                                UniqueId= Guid.NewGuid().ToString()
+                            }
+                            ,
+                            new Asignatura{Nombre="Programación",
+                                UniqueId= Guid.NewGuid().ToString()
+                            }
+                };
+            ViewBag.Fecha = DateTime.Now;
+            return View("MultiAsignatura",listaAsignaturas);
+        }
     }
 }
