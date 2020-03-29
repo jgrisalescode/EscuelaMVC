e462fe62aceebbdf651a0c7afab3f628e0eaab52
JuliaGrisales <jgrisales@gmail.com>
2020-03-29 12:46:08 +0200

    Lección 1: Revisando estructura de carpetas

--- Startup.cs
+++ Startup.cs
@@ -50,7 +50,7 @@ namespace EscuelaMVC
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Home}/{action=Index}/{id?}"); // Estilo de rutas para el site
             });
         }
     }
